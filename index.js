44a66ce02a28de4241a5523f791b9e9f54ade0ec
ashish kasma <ashish@lucentinnovation.com>
2021-05-11 21:38:10 +0530

    axios

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const { default: axios } = require('axios');
 const { Console } = require('console');
 var https = require('https');
 const cron = require("node-cron");
 
 var player = require('play-sound')(opts = {})
+
 
 format = function date2str(x, y) {
     var z = {
@@ -21,31 +23,34 @@ format = function date2str(x, y) {
     });
 }
 
+axios.defaults.headers = {
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+};
+
 function getVaccine(distId) {
-    const today = new Date();
-    console.log(today);
     const todate = new Date();
-    for (let daystart = 0; daystart < 7; daystart++) {
-        todate.setDate(today.getDate() + daystart);
-        let vDate = format(todate, 'dd-MM-yyyy');
-        var url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" + distId + "&date=" + vDate;
-        https.get(url, (res) => {
-            let body = "";
-
-            res.on("data", (chunk) => {
-                body += chunk;
-            });
-
-            res.on("end", () => {
+    let vDate = format(todate, 'dd-MM-yyyy');
+    var url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" + distId + "&date=" + vDate + "&t=" + Math.random(1);
+    console.log(todate);
+    axios.get(url)
+        .then((response) => {
+            if (response.status == 200) {
+                let body = response.data;
                 try {
-                    let centersJson = JSON.parse(body);
+                    let centersJson = body;
                     centersJson.centers.forEach(function (center, i) {
                         center.sessions.forEach(function (session, j) {
-                            if (session.min_age_limit == 18 && session.available_capacity != 0) {
+                            if (session.min_age_limit == 18 && session.available_capacity > 0) {
+                                console.log("******************************");
                                 console.log("Center Name : " + center.name);
+                                console.log("Center Address : " + center.address);
                                 console.log("Date : " + session.date);
-                                console.log("Availability: " + session.available_capacity);
-                                console.log("Slot: " + session.slots);
+                                console.log("Availability : " + session.available_capacity);
+                                console.log("Slot : " + session.slots);
+                                console.log("Vaccine Type : " + session.vaccine);
                                 player.play('./assets/assets_test.mp3', function (err) {
                                     if (err) throw err
                                 });
@@ -53,16 +58,15 @@ function getVaccine(distId) {
                         })
                     });
                 } catch (error) {
-                    console.error(error.message);
+                    console.error(error);
                 };
-            });
-
-        }).on("error", (error) => {
-            console.error(error.message);
-        });
-    }
+            }
+        }).catch(error => {
+            console.log(error)
+        })
 }
 
 cron.schedule("*/60 * * * * *", function () {
     getVaccine(770);
+    Object.keys(require.cache).forEach(function (key) { delete require.cache[key] })
 });
